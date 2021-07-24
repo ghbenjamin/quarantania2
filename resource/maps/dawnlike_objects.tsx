b598d6f178b87a86d950008919c274b5dad0cf63
Benjamin Hammond <brh1316@gmail.com>
2021-07-24 13:34:25 +0100

    Added individual dawnlike sprites

--- resource/maps/dawnlike_objects.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="dawnlike_objects" tilewidth="32" tileheight="32" tilecount="928" columns="32" objectalignment="topleft">
- <image source="../spritesheet/dawnlike_objects.png" width="1024" height="928"/>
-</tileset>
