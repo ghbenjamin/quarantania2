08d0a9f56c918e9c0508a515a30d94abd8b87c5a
Benjamin Hammond <brh1316@gmail.com>
2021-07-26 18:17:24 +0100

    Moved map data to more sensible place

--- tools/maps/tilesets/dawnlike.tsx
+++ tools/maps/tilesets/dawnlike.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.1" name="dawnlike" tilewidth="32" tileheight="32" spacing="4" margin="1" tilecount="3080" columns="56" objectalignment="topleft">
- <image source="../spritesheet/dawnlike.png" width="2014" height="1978"/>
+ <image source="../../../resource/spritesheet/dawnlike.png" width="2014" height="1978"/>
 </tileset>
