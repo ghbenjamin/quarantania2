8af9027dd611f166748aef92d30481de5787de79
Benjamin Hammond <brh1316@gmail.com>
2022-04-12 23:06:51 +0100

    Working build on Win11

--- src/graphics/Window.cpp
+++ src/graphics/Window.cpp
@@ -9,7 +9,7 @@ Window::Window(GlobalConfig::GlobalConfigInfo const& info)
     SDL_Init(SDL_INIT_EVERYTHING);
 
     SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
